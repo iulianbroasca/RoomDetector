efea2e37ea6081c37a42c3b09222f90b5a0e35e0
Broască Ioan Iulian <iulianbroasca@yahoo.ro>
2020-06-24 19:58:03 +0300

    Added files/scripts

---
+++ Files/B/Scripts/Loader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class Loader : MonoBehaviour
+    {
+        public float speed = 50;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.back * (speed * Time.deltaTime));
+        }
+    }
+}
---
+++ Files/B/Scripts/UIManagerMainScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class UIManagerMainScene : MonoBehaviour
+    {
+        public void LoadScene(int number)
+        {
+            SceneManager.LoadScene(number);
+        }
+    }
+}
